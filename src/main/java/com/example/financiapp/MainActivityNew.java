c59c292025e1fae4c01cd625ea334724e1c1fc6f
Jose Martín <81288925+josemr1979@users.noreply.github.com>
2024-05-19 11:55:52 +0200

    Add files via upload

---
+++ src/main/java/com/example/financiapp/MainActivityNew.java
@@ -0,0 +1,14 @@
+package com.example.financiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityNew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_new);
+    }
+}
